6c78eb493a063d3b802740f1d0a6316005b0db0e
PhoenixWANG <1493736059@qq.com>
2023-04-15 19:06:05 +0800

    Update lsqApproximationOverdamped.m

--- lsqApproximationOverdamped.m
+++ lsqApproximationOverdamped.m
@@ -1,3 +1,8 @@
+%ME31002 MiniProject
+%WANG Dapeng Phoenix 20074734d Department of Mechanical Engineering
+%THE HONG KONG POLYTECHNIC UNIVERSITY
+%Contact: 20074734d@connect.polyu.hk
+
 
 function [k, b] = lsqApproximationOverdamped(filename,k,b1)
     % 导入数据
